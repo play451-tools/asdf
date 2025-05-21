d4376dcef7cd0af3320e095704835c909cd0d744
Jmate-2011 <7b7t.net@gmail.com>
2025-05-21 13:55:36 +0000

    lets go

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
-use sysinfo::System;
+use sysinfo::{System, RefreshKind, CpuRefreshKind};
+use prettytable::{Table, format::consts::FORMAT_NO_LINESEP, row};
+use crossterm::{
+    event::EnableMouseCapture,
+    execute,
+    queue,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    terminal::{Clear, ClearType},
+};
+use std::io::stdout;
 
 fn main() {
-    let mut system = System::new();
-    system.refresh_memory();
-    
-    println!("\n\t\t\tRAM Usage");
-    println!("\t+-----------------------------------+");
-    println!(
-        "\t| Used: \x1B[92m{:.2} GB\x1B[0m / Total: \x1B[93m{:.2} GB\x1B[0m |",
-        system.used_memory() as f64 / 1024.0 / 1024.0 / 1024.0,
-        system.total_memory() as f64 / 1024.0 / 1024.0 / 1022.0
+    let mut system = System::new_with_specifics(
+        RefreshKind::nothing().with_cpu(CpuRefreshKind::everything()),
     );
-    println!("\t+-----------------------------------+\n");
+    system.refresh_memory();
+    system.refresh_cpu_all();
+    
+    let cpus: Vec<_> = system.cpus().into_iter().collect();
+    if let Some(first_cpu) = cpus.first() {
+        let memory_used = system.used_memory() as f64 / 1024.0 / 1024.0 / 1024.0;
+        let memory_total = system.total_memory() as f64 / 1024.0 / 1024.0 / 1024.0;
+        
+        let mut table = Table::new();
+        table.set_format(*FORMAT_NO_LINESEP);
+        table.set_titles(row![bF=> "RAM", "CPU"]);
+        
+        table.add_row(row![bF=> 
+            format!("Used: {:.2} GB / Total: {:.2} GB", memory_used, memory_total),
+            format!("Brand: {} ({} cores)", 
+                first_cpu.brand(), 
+                cpus.len(), 
+                )
+        ]);
+        
+        let mut stdout = stdout();
+        queue!(
+            stdout,
+            Clear(ClearType::All),
+            SetForegroundColor(Color::Green),
+            Print(format!("{}", table)),
+            ResetColor,
+        ).unwrap();
+    }
 }
